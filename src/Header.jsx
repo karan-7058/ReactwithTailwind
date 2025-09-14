94772eb4f989b8e83d99b617207178fa34de8f2d
karan-7058 <aghavkaran164@gmail.com>
2025-09-14 11:39:16 +0530

    header css is optimized

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,16 @@
 function Header(){
     return (
         <>
+          <div className="bg-gray-200 p-4 shadow-md h-30 sticky top-0 z-10">
             <h1 className="text-3xl font-bold underline text-center mt-4">
                 Welcome to React with Tailwind CSS
             </h1>
             <p className="text-center mt-2">
                 This is a simple card layout using React and Tailwind CSS.
             </p>
-            <hr className="my-4" />     
+            <hr className="my-2" />  
 
+            </div>
         </>
     )
 }
